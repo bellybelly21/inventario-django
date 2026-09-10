e1514c579f441dbe62df3778eadaff30dcd95837
bellybelly21 <sybellasss@gmail.com>
2026-09-09 21:22:25 -0300

    def editar y eliminar productos + templates

--- inventario/productosApp/views.py
+++ inventario/productosApp/views.py
@@ -4,18 +4,40 @@ from .forms import ProductoForm
 
 def listar_productos(request):
     productos = Producto.objects.all()
-
     return render(request, 'productosApp/productos.html', {'productos': productos})
 
 
 def agregar_producto(request):
-    if(request.method == 'POST'):
+    if request.method == 'POST':
         form = ProductoForm(request.POST)
-
-        if (form.is_valid()):
+        if form.is_valid():
             form.save()
             return redirect('productos')
     else:
         form = ProductoForm()
 
-    return render (request, 'productosApp/formulario.html', {'form':form, 'titulo': 'Agregar Producto'})
+    return render(request, 'productosApp/formulario.html', {'form': form, 'titulo': 'Agregar Producto'})
+
+
+def editar_producto(request, producto_id):
+    producto = get_object_or_404(Producto, id=producto_id)
+    
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, instance=producto)
+        if form.is_valid():
+            form.save()
+            return redirect('productos')
+    else:
+        form = ProductoForm(instance=producto)
+    
+    return render(request, 'productosApp/formulario.html', {'form': form, 'titulo': 'Editar Producto'})
+
+
+def eliminar_producto(request, producto_id):
+    producto = get_object_or_404(Producto, id=producto_id)
+    
+    if request.method == 'POST':
+        producto.delete()
+        return redirect('productos') 
+      
+    return render(request, 'productosApp/eliminar.html', {'producto': producto}) 
